0f3f988861ce52fafacedcc308f3c866add18a54
XZ361 <2457307263@qq.com>
2024-05-20 10:33:07 +0800

    feat: add 响应拦截器

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import '@/styles/index.scss'
 
 import SvgIcon from '@/icons'
 // import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 SvgIcon(app)
